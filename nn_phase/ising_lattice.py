7d3bff33937e57fa81cebbd87f8e796c6969acd2
Kuo Ting-Kai <nehsm30126@gmail.com>
2020-02-20 01:33:44 +0800

    mc_step, training, confusion code

---
+++ nn_phase/ising_lattice.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+from numba import njit, prange
+
+# Gives random spin configuration
+def randomstate(N):
+    return 2 * np.random.randint(2, size=(N, N)) - 1
+
+# Performs one Metropolis step
+@njit(parallel=True) # simple parallel for loop
+def metropolis_step(lattice, T):
+    for _ in prange(N * N):
+        x, y = np.random.randint(0, N), np.random.randint(0, N)
+        deltaE = 2 * lattice[x, y] * (lattice[(x + 1) % N, y] + lattice[x, (y + 1) % N] + # Periodic boundary conditions
+                       lattice[(x - 1) % N, y] + lattice[x, (y - 1) % N])
+        if deltaE < 0:
+            lattice[x, y] *= -1
+        elif np.random.rand() < np.exp(-deltaE / T):
+            lattice[x, y] *= -1
+
+# Saves data
+def save(object, filename):
+    with open(filename + '.pickle', 'wb') as f:
+        pickle.dump(object, f)
+
+
+# Setting up simulation parameters
+N = 16
+Tc = 2.269
+MCsteps = 5000
+EQsteps = 2000
+ntemp = 1000
+
+T = np.linspace(1., 3.5, ntemp)
+magnetization = []
+
+# Initializing spins and labels
+spins, labels = np.zeros((0, N * N)), np.zeros((0, 2))
+high, low = np.array([1, 0]), np.array([0, 1])
+
+# Perform simulation
+for index, temp in enumerate(T):
+    tmp = []
+    lattice = randomstate(N)
+
+    # Equilibrate spin lattice
+    for _ in range(EQsteps):
+        metropolis_step(lattice, temp)
+
+    # Loop over spin configurations to collect data
+    for mc in range(MCsteps):
+        if mc % 200 == 0:
+            tmp.append(np.sum(lattice))
+        metropolis_step(lattice, temp)
+    spins = np.vstack((spins, lattice.ravel()))
+
+    # Append correct label corresponding to current spin configuration
+    if temp < Tc:
+        labels = np.vstack((labels, low))
+    else:
+        labels = np.vstack((labels, high))
+
+    magnetization.append(np.mean(tmp) / (N * N))
+    print('{} out of {} temperature steps'.format(index, len(T)))
+
+# Save data
+save(0.5 * (spins + 1), 'train_spins'), save(labels, 'train_labels'), save(T, 'temperature')
+print("saved data!")
+
+# Plot Monte Carlo simulation of magnetization
+plt.plot(T, abs(np.array(magnetization)), 'o', color="green")
+plt.xlabel("Temperature", fontsize=15)
+plt.ylabel("Magnetization ", fontsize=15)
+plt.grid()
+plt.savefig('magnetization_MC.png')
+
+
---
+++ nn_phase/neural_net_ising.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+import tensorflow.compat.v1 as tf # for compability
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+tf.disable_v2_behavior() # for copability
+# Todo: hyperparameter tuning
+
+# Seed random number generator
+random_state = 42
+np.random.seed(random_state)
+#tf.random.set_seed(random_state) 
+tf.set_random_seed(random_state) #in tf ver 1.x
+
+# Function to load training and test data
+def load(filename):
+    with open(filename + '.pickle', 'rb') as f:
+        return pickle.load(f)
+
+# Loading data
+data_x, data_y = load('train_spins'), load('train_labels')
+T = load('temperature')
+
+# Split data into training and test set
+train_x, test_x, train_y, test_y, train_T, test_T = train_test_split(data_x, data_y, T, test_size=0.1, random_state=random_state)
+
+# Parameters
+learning_rate = 1e-2
+l2 = 2 * 1e-5
+training_epochs = 5000
+display_step = 100
+
+# Network Parameters
+n_hidden_1 = 100 # 1st layer number of features
+n_input = 16 * 16 # 2D Ising lattice
+n_classes = 2 # high and low phase
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'out': tf.Variable(tf.random_normal([n_hidden_1, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.constant(0.1, shape=[n_hidden_1])),
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes]))
+}
+
+# Create model
+def singlelayer_sigmoid(x, weights, biases):
+    layer_1 = tf.sigmoid(tf.matmul(x, weights['h1']) + biases['b1'])
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    return out_layer
+
+# Construct model
+pred = singlelayer_sigmoid(x, weights, biases)
+
+# Define cost and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=y))
+regularizer = tf.nn.l2_loss(weights['h1']) + tf.nn.l2_loss(weights['out'])
+cost = tf.reduce_mean(cost + l2 * regularizer)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Initializing the variables
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# Training loop
+for epoch in range(training_epochs):
+
+    # Randomly shuffle training data
+    perm = np.arange(len(train_x))
+    np.random.shuffle(perm)
+    train_x, train_y = train_x[perm], train_y[perm]
+
+    # Training step
+    _, c = sess.run([optimizer, cost], feed_dict={x: train_x, y: train_y})
+
+    # Display logs per epoch step
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch + 1), "Cost =", "{:.3f}".format(c))
+
+# Test model
+correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+
+# Calculate and print accuracy
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print('Network trained!')
+print('Accuracy: {:.3f}'.format(accuracy.eval({x: test_x, y: test_y})))
+
+# Define NN output
+output = tf.nn.softmax(singlelayer_sigmoid(tf.cast(test_x, tf.float32), weights, biases))
+
+# Plotting output of NN
+plt.plot(test_T, abs(output[:, 0].eval()), '+', color="green", label='high T')
+plt.plot(test_T, abs(output[:, 1]).eval(), 'v', color="red", label='low T')
+plt.ylim(-0.05, 1.05)
+plt.xlabel("Temperature", fontsize=15)
+plt.ylabel("Output ", fontsize=15)
+plt.grid()
+plt.legend()
+plt.savefig('magnetization_NN.png')
